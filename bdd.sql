9ca76bcd1396864b2c1d6c438ac08a3a702d1f66
lroux07 <lroux@edenschool.fr>
2025-01-28 23:43:19 +0100

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 14 jan. 2025 à 15:47
+-- Généré le : mar. 28 jan. 2025 à 22:41
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -31,19 +31,29 @@ DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
   `id_article` int NOT NULL AUTO_INCREMENT,
   `image_article` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `nom_article` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `description` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nom_article` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_modele` int NOT NULL,
+  `prix` int NOT NULL,
   `no_serie` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `date_ajout` datetime NOT NULL,
-  `id_modele` int NOT NULL,
   `id_utilisateur` int NOT NULL,
   `stock` tinyint(1) NOT NULL,
+  `stock_restant` int NOT NULL,
   PRIMARY KEY (`id_article`),
   UNIQUE KEY `Nom_article` (`nom_article`),
   UNIQUE KEY `No série` (`no_serie`),
   KEY `id_modele` (`id_modele`),
   KEY `id_utilisateur` (`id_utilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `articles`
+--
+
+INSERT INTO `articles` (`id_article`, `image_article`, `nom_article`, `description`, `id_modele`, `prix`, `no_serie`, `date_ajout`, `id_utilisateur`, `stock`, `stock_restant`) VALUES
+(1, 'ecran.png', 'Ecran PC - HP V27i - 27\" FHD - Dalle IPS - 5 ms - 60 Hz - HDMI / VGA', '* Type d\'affichageEcran LCD à rétroéclairage LED / matrice active TFT\n* InterfacesVGA, 3 x USB 3.2 Gen 1, HDMI\n* Résolution nativeFull HD (1080p) 1920 x 1080 à 60 Hz\n* Temps de réponse5 ms (gris-à-gris)', 1, 22, '1', '2025-01-14 19:06:18', 2, 1, 20),
+(4, 'clavier.png', 'Clavier gaming sans fil - ASUS - ROG AZOTH - RGB - N Key Rollover - Programmable', '* Clavier\n* InterfaceUSB 2.0, Bluetooth 5.1, RF 2.4GHz\n* Fonction de raccourcis clavierAll Keys Programmable\n* TechnologieSans fil', 1, 50, '2', '2025-01-28 21:13:46', 2, 1, 10);
 
 -- --------------------------------------------------------
 
@@ -76,7 +86,14 @@ CREATE TABLE IF NOT EXISTS `modeles` (
   `id_modele` int NOT NULL AUTO_INCREMENT,
   `modele` varchar(20) NOT NULL,
   PRIMARY KEY (`id_modele`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `modeles`
+--
+
+INSERT INTO `modeles` (`id_modele`, `modele`) VALUES
+(1, 'Test');
 
 -- --------------------------------------------------------
 
@@ -89,7 +106,15 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `id_role` int NOT NULL AUTO_INCREMENT,
   `role` varchar(20) NOT NULL,
   PRIMARY KEY (`id_role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `roles`
+--
+
+INSERT INTO `roles` (`id_role`, `role`) VALUES
+(1, 'Administrateur'),
+(2, 'Utilisateur');
 
 -- --------------------------------------------------------
 
@@ -100,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
 DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `id_utilisateur` int NOT NULL AUTO_INCREMENT,
-  `image` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `image` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `nom_utilisateur` varchar(20) NOT NULL,
   `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `numero_telephone` int NOT NULL,
@@ -114,7 +139,14 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   UNIQUE KEY `nom_utilisateur` (`nom_utilisateur`),
   UNIQUE KEY `Email` (`email`),
   KEY `id_role` (`id_role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_utilisateur`, `image`, `nom_utilisateur`, `email`, `numero_telephone`, `password`, `adresse`, `ville`, `code_postal`, `date_inscription`, `id_role`) VALUES
+(2, NULL, 'Laélian', 'laelian.roux@gmail.c', 662720545, 'laelian', '85 rue Docteur Frapp', 'Villeurbanne', '69100', '2025-01-14 19:05:13', 1);
 
 --
 -- Contraintes pour les tables déchargées
