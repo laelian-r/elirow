f3d89652f42091b727edf431a6ad498bc3bda8ba
lroux07 <lroux@edenschool.fr>
2025-02-11 17:34:49 +0100

    Finish register

--- pages/verif_register.php
+++ pages/verif_register.php
@@ -1,10 +1,16 @@
 <?php
 session_start();
+include '../components/db_connect.php';
 
 if (isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['mdp'])) {
     if (!empty($_POST['pseudo']) && $_POST['pseudo'] >= 3 && !empty($_POST['email']) && !empty($_POST['mdp'])) {
         session_destroy();
-        header('Location: ../index.php');
+
+        $sql = "INSERT INTO utilisateurs (nom_utilisateur, email, password) VALUES (:pseudo, :email, :mdp)";
+        $resultat = $db->prepare($sql);
+        $resultat->execute(['pseudo' => $_POST["pseudo"], 'email' => $_POST["email"], 'mdp' => $_POST["mdp"]]);
+
+        header('Location: ./index.php');
     } else {
         $_SESSION['old'] = $_POST;
         header('Location: ./register.php');
