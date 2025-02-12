a9b72bdcc48092461460c1506ae1aaeab8ab3396
lroux07 <lroux@edenschool.fr>
2025-02-12 15:09:25 +0100

    Complete register

--- pages/register.php
+++ pages/register.php
@@ -16,14 +16,29 @@
         <form action="./verif_register.php" method="post" class="auth-form" enctype="multipart/form-data">
             <h2>Inscription</h2>
 
-            <label for="pseudo">Nom d'utilisateur</label>
-            <input type="text" name="pseudo" id="pseudo" value="<?php if(isset($_SESSION['old'])){ echo $_SESSION['old']['pseudo'];} ?>">
+            <label for="image">Photo de profil</label>
+            <input type="file" name="image" id="image">
+
+            <label for="username">Nom d'utilisateur</label>
+            <input type="text" name="username" id="username" value="<?php if(isset($_SESSION['old'])){ echo $_SESSION['old']['username'];} ?>">
+
+            <label for="phone">Numéro de téléphone</label>
+            <input type="tel" name="phone" id="phone" value="<?php if(isset($_SESSION['old'])) { echo $_SESSION['old']['phone'];} ?>">
 
             <label for="email">Addresse email</label>
             <input type="email" name="email" id="email" value="<?php if(isset($_SESSION['old'])) { echo $_SESSION['old']['email'];} ?>">
 
             <label for="mdp">Mot de passe</label>
             <input type="password" name="mdp" id="mdp" value="<?php if(isset($_SESSION['old'])) { echo $_SESSION['old']['mdp'];} ?>">
+
+            <label for="address">Adresse</label>
+            <input type="text" name="address" id="address" value="<?php if(isset($_SESSION['old'])) { echo $_SESSION['old']['address'];} ?>">
+
+            <label for="city">Ville</label>
+            <input type="text" name="city" id="city" value="<?php if(isset($_SESSION['old'])) { echo $_SESSION['old']['city'];} ?>">
+
+            <label for="postal_code">Code postal</label>
+            <input type="text" name="postal_code" id="postal_code" value="<?php if(isset($_SESSION['old'])) { echo $_SESSION['old']['postal_code'];} ?>">
 
             <input type="submit" name="submit" value="S'incrire">
         </form>
--- pages/verif_register.php
+++ pages/verif_register.php
@@ -2,13 +2,32 @@
 session_start();
 include '../components/db_connect.php';
 
-if (isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['mdp'])) {
-    if (!empty($_POST['pseudo']) && $_POST['pseudo'] >= 3 && !empty($_POST['email']) && !empty($_POST['mdp'])) {
+if (
+    isset($_POST['username']) && isset($_POST['phone']) && isset($_POST['email']) && 
+    isset($_POST['mdp']) && isset($_POST['address']) && isset($_POST['city']) && 
+    isset($_POST['postal_code'])
+) {
+    if (
+        !empty($_POST['username']) && $_POST['username'] >= 3 && 
+        !empty($_POST['phone']) && !empty($_POST['email']) && 
+        !empty($_POST['mdp']) && !empty($_POST['address']) &&
+        !empty($_POST['city']) && !empty($_POST['postal_code'])
+    ) {
         session_destroy();
 
-        $sql = "INSERT INTO utilisateurs (nom_utilisateur, email, password) VALUES (:pseudo, :email, :mdp)";
+        $sql = "INSERT INTO utilisateurs (image, nom_utilisateur, numero_telephone, email, password, adresse, ville, code_postal, id_role) VALUES (:image, :username, :phone, :email, :mdp, :address, :city, :postal_code, :id_role)";
         $resultat = $db->prepare($sql);
-        $resultat->execute(['pseudo' => $_POST["pseudo"], 'email' => $_POST["email"], 'mdp' => $_POST["mdp"]]);
+        $resultat->execute([
+            'image' => $_FILES['image']['name'],
+            'username' => $_POST["username"],
+            'phone' => $_POST["phone"],
+            'email' => $_POST["email"],
+            'mdp' => $_POST["mdp"],
+            'address' => $_POST["address"],
+            'city' => $_POST["city"],
+            'postal_code' => $_POST["postal_code"],
+            'id_role' => 2
+        ]);
 
         header('Location: ./index.php');
     } else {
